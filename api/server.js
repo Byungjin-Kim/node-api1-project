b5909f1f1173ee92988230b5aa0324c96456fd81
Byungjin-Kim <composerjins@gmail.com>
2023-04-26 12:13:55 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,3 +1,38 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+
+server.get('/', (req, res) => {
+    // name is not important (could be request, response), position is.
+    res.json({ hello: 'world' })
+})
+
+server.get('/api/users', (req, res) => {
+    User.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(error => {
+            res.status(500).json({ error: error.message })
+        })
+})
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            res.json(user)
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'error getting user',
+                error: error.message,
+                stack: error.stack,
+            })
+        })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
+
+
