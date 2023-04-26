04cda7a46bb27674cf9123d9ca9745f3118d0119
Byungjin-Kim <composerjins@gmail.com>
2023-04-26 12:20:31 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -22,7 +22,12 @@ server.get('/api/users', (req, res) => {
 server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
         .then(user => {
-            res.json(user)
+            if (!user) {
+                res.status(404).json({
+                    message: "The user with the specified ID does not exist"
+                })
+            }
+            res.status(200).json(user)
         })
         .catch(error => {
             res.status(500).json({
